3924247f75e35981b76f7aece07bd2a66e2d82e6
Manish Kumar <110106315+manishkmr49@users.noreply.github.com>
2024-07-27 21:01:50 +0530

    Add files via upload

---
+++ Recursion Basic/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int N = 10;
+        System.out.println("The " + N + "th Fibonacci number is: " + fibonacci(N));
+    }
+}
