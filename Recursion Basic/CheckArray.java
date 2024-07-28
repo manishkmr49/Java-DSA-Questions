ca712862ce9b95dd5d097b66c3f5548745b297fd
Manish Kumar <110106315+manishkmr49@users.noreply.github.com>
2024-07-28 14:45:17 +0530

    Add files via upload

---
+++ Recursion Basic/CheckArray.java
@@ -0,0 +1,17 @@
+public class CheckArray {
+    
+    public static boolean IsSorted(int arr[], int i) {
+        if(i == arr.length-1) {
+            return true;
+        }
+        if(arr[i] > arr[i+1]) {
+            return false;
+        }
+        return IsSorted(arr, i+1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        System.out.print(IsSorted(arr, 0));
+    }
+}
