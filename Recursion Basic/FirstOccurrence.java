47c897c50436d39457ae61c727b189cf45c39c7c
Manish Kumar <110106315+manishkmr49@users.noreply.github.com>
2024-07-30 12:08:28 +0530

    Update FirstOccurrence.java

--- Recursion Basic/FirstOccurrence.java
+++ Recursion Basic/FirstOccurrence.java
@@ -1,32 +1,18 @@
 public class FirstOccurrence {
 
-    public static int findFirstOccurrence(int[] array, int value) {
-        return findFirstOccurrenceRecursive(array, value, 0);
-    }
+    public static int FindOccurence(int arr[], int key, int i){
 
-    private static int findFirstOccurrenceRecursive(int[] array, int value, int index) {
-        // Base case: if index is out of bounds, return -1
-        if (index >= array.length) {
+        if(i == arr.length){
             return -1;
         }
-        // Check if the current element matches the value
-        if (array[index] == value) {
-            return index;
+        if(arr[i] ==  key){
+            return i;
         }
-        // Recursive case: check the next index
-        return findFirstOccurrenceRecursive(array, value, index + 1);
+        return FindOccurence(arr, key, i+1);
     }
-
     public static void main(String[] args) {
-        int[] array = {2, 4, 6, 8, 4, 10};
-        int valueToFind = 4;
-        
-        int result = findFirstOccurrence(array, valueToFind);
-        
-        if (result != -1) {
-            System.out.println("The first occurrence of " + valueToFind + " is at index: " + result);
-        } else {
-            System.out.println(valueToFind + " is not present in the array.");
-        }
+        int arr[]= {8,3,6,9,5,10,2,5,3};
+        int key = 5;
+        System.out.println(FindOccurence(arr, key, 0));
     }
 }
